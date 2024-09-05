70dd9ca59f6de8335345e0273e347e490b93d723
int16 <int16int8@gmail.com>
2024-09-05 06:36:39 +0000

    init: Initial commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module status
+
+go 1.22.6
+
+require (
+	github.com/gonebot-dev/gonebot v0.1.2
+	github.com/shirou/gopsutil v3.21.11+incompatible
+	github.com/shirou/gopsutil/v3 v3.24.5
+)
+
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/tklauser/go-sysconf v0.3.14 // indirect
+	github.com/tklauser/numcpus v0.8.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,86 @@
+package status
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+
+	"github.com/gonebot-dev/gonebot/messages"
+	"github.com/gonebot-dev/gonebot/plugins"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/v3/mem"
+)
+
+type sysInfo struct {
+	//Mem
+	MemAll         uint64
+	MemFree        uint64
+	MemUsed        uint64
+	MemUsedPercent float64
+	//Boot time
+	Days    int64
+	Hours   int64
+	Minutes int64
+	Seconds int64
+	//CPU
+	CpuUsedPercent float64
+	CpuCores       int
+	CpuInfo        string
+	//OS
+	OS   string
+	Arch string
+}
+
+func GetSysInfo() (info sysInfo) {
+	//Mem
+	v, _ := mem.VirtualMemory()
+	info.MemAll = v.Total
+	info.MemFree = v.Free
+	info.MemUsed = info.MemAll - info.MemFree
+	info.MemUsedPercent = float64(info.MemUsed) / float64(info.MemAll) * 100.0
+	unit := uint64(1024 * 1024) // MB
+	info.MemAll /= unit
+	info.MemUsed /= unit
+	info.MemFree /= unit
+	//CPU
+	info.CpuCores = runtime.GOMAXPROCS(0)
+	cc, _ := cpu.Percent(time.Millisecond*200, false) //CPU usage in 200ms
+	info.CpuUsedPercent = cc[0]
+	//OS
+	info.OS = runtime.GOOS
+	info.Arch = runtime.GOARCH
+
+	// 获取开机时间
+	boottime, _ := host.BootTime()
+	ntime := time.Now().Unix()
+	btime := time.Unix(int64(boottime), 0).Unix()
+	deltatime := ntime - btime
+
+	info.Seconds = int64(deltatime)
+	info.Minutes = info.Seconds / 60
+	info.Seconds -= info.Minutes * 60
+	info.Hours = info.Minutes / 60
+	info.Minutes -= info.Hours * 60
+	info.Days = info.Hours / 24
+	info.Hours -= info.Days * 24
+	return
+}
+
+func handler(msg messages.IncomingStruct) (result messages.ResultStruct) {
+	info := GetSysInfo()
+	result.Text = fmt.Sprintf("CPU usage: %f\n", info.CpuUsedPercent)
+	return result
+}
+
+func init() {
+	status := plugins.GonePlugin{}
+	status.Name = "status"
+	status.Description = "Picture status"
+	status.Handlers = append(status.Handlers,
+		plugins.GoneHandler{
+			Command: []string{"status", "状态", "stat"},
+			Handler: handler,
+		})
+	plugins.LoadPlugin(status)
+}
