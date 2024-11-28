34b00a383e58eb1b6b4193f450b813ebc2ac6b41
Kingcq <404291187@qq.com>
2024-11-28 15:47:11 +0800

    feat: update gonebot version to v1.0.1

--- status.go
+++ status.go
@@ -1,30 +1,16 @@
 package status
 
 import (
-	"reflect"
-
 	"github.com/gonebot-dev/gonebot/adapter"
 	"github.com/gonebot-dev/gonebot/message"
 	"github.com/gonebot-dev/gonebot/plugin"
-	"github.com/gonebot-dev/gonebot/rule"
+	"github.com/gonebot-dev/gonebot/plugin/rule"
 	"github.com/gonebot-dev/goneplugin-status/renderer"
 )
 
 func handler(a *adapter.Adapter, msg message.Message) bool {
-	imageSerializer := message.GetSerializer("image", "")
-	imageSegment := message.MessageSegment{
-		Type: "image",
-		Data: imageSerializer.Serialize(message.ImageType{
-			File: renderer.Render(a),
-		}, reflect.TypeOf(imageSerializer)),
-	}
-	var result message.Message
-	result.Sender = msg.Self
-	result.Receiver = msg.Sender
-	result.Group = msg.Group
-	result.Self = msg.Self
-	result.Attach(imageSegment, imageSerializer)
-	a.SendChannel.Push(result, false)
+	result := message.NewReply(msg).Image(renderer.Render(a))
+	a.SendMessage(result)
 	return true
 }
 
@@ -36,7 +22,7 @@ func init() {
 	Status.Description = "Show bot status"
 
 	Status.Handlers = append(Status.Handlers, plugin.GoneHandler{
-		Rules:   []rule.FilterBundle{{Filters: []rule.FilterRule{rule.ToMe(), rule.Command([]string{"status"})}}},
+		Rules:   rule.NewRules(rule.ToMe()).And(rule.Command("status")),
 		Handler: handler,
 	})
 }
